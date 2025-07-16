42cff2b028a124434a445fe735c937a1b726f0a9
adrian-luque <adrian.luque@perondagroup.com>
2025-07-16 09:00:23 +0200

    Refactor HomePage component to remove unused imports and simplify navigation using Link

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,7 +1,3 @@
-"use client";
-
-import { useState, useRef } from "react";
-import { useRouter } from "next/navigation";
 import {
   Card,
   CardContent,
@@ -9,24 +5,10 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { Check, Copy } from "lucide-react";
+import Link from "next/link";
 
 const HomePage = () => {
-  const [isCopied, setIsCopied] = useState(false);
-  const textToCopyRef = useRef(null);
-  const router = useRouter();
-
-  const handleCopy = async () => {
-    try {
-      await navigator.clipboard.writeText(textToCopyRef.current.textContent);
-      setIsCopied(true);
-      setTimeout(() => setIsCopied(false), 2000); // Reset after 2 seconds
-    } catch (err) {
-      console.error("Failed to copy text: ", err);
-    }
-  };
 
   const stockCategories = [
     { name: "Peronda", route: "/home/stock/99" },
@@ -41,19 +23,20 @@ const HomePage = () => {
         <h2 className="text-3xl font-bold text-center mb-8">Stock Categories</h2>
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
           {stockCategories.map((category) => (
-            <Card
-              key={category.name}
-              className="hover:shadow-lg transition-shadow cursor-pointer"
-              onClick={() => router.push(category.route)}
-            >
-              <CardHeader>
-                <CardTitle>{category.name}</CardTitle>
-                <CardDescription>View stock for {category.name}</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <Button variant="outline" className={"cursor-pointer"}>Go to Stock</Button>
-              </CardContent>
-            </Card>
+              <Card
+                className="hover:shadow-lg transition-shadow"
+                key={category.name}
+              >
+                <CardHeader>
+                  <CardTitle>{category.name}</CardTitle>
+                  <CardDescription>View stock for {category.name}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <Link href={category.route} >
+                    <Button variant="outline" className={"cursor-pointer"}>Go to Stock</Button>
+                  </Link>
+                </CardContent>
+              </Card>
           ))}
         </div>
       </section>
