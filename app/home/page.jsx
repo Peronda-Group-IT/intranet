ca5d182dd0353ecec5d0e4724f4dd91974694416
adrian-luque <adrian.luque@perondagroup.com>
2025-08-05 09:51:58 +0200

    style: home card button deleted

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -10,7 +10,6 @@ import Link from "next/link";
 import { loadTranslations } from "@/lib/server-utils";
 
 const HomePage = async () => {
-
   const stockCategories = [
     { name: "Peronda", route: "/home/stock/99" },
     { name: "Discontinuados", route: "/home/stock/97" },
@@ -23,23 +22,24 @@ const HomePage = async () => {
   return (
     <div className="container mx-auto p-4 space-y-8">
       <section id="stock-categories">
-        <h2 className="text-3xl font-bold text-center mb-8">{translations['stock-categories']}</h2>
+        <h2 className="text-3xl font-bold text-center mb-8">
+          {translations["stock-categories"]}
+        </h2>
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
           {stockCategories.map((category) => (
+            <Link href={category.route} key={category.name}>
               <Card
                 className="hover:shadow-lg transition-shadow"
-                key={category.name}
               >
                 <CardHeader>
                   <CardTitle>{category.name}</CardTitle>
-                  <CardDescription>{translations['view-stock-for']} {category.name}</CardDescription>
+                  <CardDescription>
+                    {translations["view-stock-for"]} {category.name}
+                  </CardDescription>
                 </CardHeader>
-                <CardContent>
-                  <Link href={category.route} >
-                    <Button variant="outline" className={"cursor-pointer w-full"}>{translations["go-to-stock"]}</Button>
-                  </Link>
-                </CardContent>
+                <CardContent></CardContent>
               </Card>
+            </Link>
           ))}
         </div>
       </section>
