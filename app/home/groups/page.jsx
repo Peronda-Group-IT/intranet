aa59d54cec660cd8bd5027d1c3c6950337c26681
adrian-luque <adrian.luque@perondagroup.com>
2025-10-15 13:11:41 +0200

    feat(groups): possibility to delete a group from the list and groups are fetched from database and showd in home screen

--- app/home/groups/page.jsx
+++ app/home/groups/page.jsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/table";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { CreateGroupDialog } from "@/components/groups/create-group-dialog";
+import { DeleteGroupDialog } from "@/components/groups/delete-group-dialos";
 
 
 export default async function GroupsPage() {
@@ -18,8 +19,8 @@ export default async function GroupsPage() {
     const translations = await loadTranslations();
 
     return (
-        <div className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 max-w-6xl mx-auto">
-            <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
+        <div className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 max-w-6xl mx-auto h-full">
+            <Card className="sm:col-span-2 h-full" x-chunk="dashboard-05-chunk-0">
                 <CardHeader className="pb-3 items-center">
                     <div className="flex flex-row justify-between w-full">
                         <div>
@@ -31,12 +32,14 @@ export default async function GroupsPage() {
                         <CreateGroupDialog />
                     </div>
                 </CardHeader>
-                <CardContent>
+                <CardContent className={"flex justify-center h-full"}>
+                    {groups.length > 0 ? (
                     <Table>
                         <TableHeader>
                             <TableRow>
                                 <TableHead>{translations["groups_id_header"]}</TableHead>
                                 <TableHead>{translations["groups_name_header"]}</TableHead>
+                                <TableHead className={"w-20"}></TableHead>
                             </TableRow>
                         </TableHeader>
                         <TableBody>
@@ -44,10 +47,17 @@ export default async function GroupsPage() {
                                 <TableRow key={group.id}>
                                     <TableCell>{group.id}</TableCell>
                                     <TableCell>{group.name}</TableCell>
+                                    <TableCell>
+                                        <div className="flex justify-end">
+                                            <DeleteGroupDialog group={group}/>
+                                        </div>
+                                    </TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
-                    </Table>
+                    </Table>) : (
+                        <p className="text-center text-muted-foreground my-auto">No groups available.</p>
+                    )}
                 </CardContent>
             </Card>
         </div>
